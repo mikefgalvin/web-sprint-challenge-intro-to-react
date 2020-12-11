516f6ed714e2b183c2ed2819417d77a48d014cfd
Mike <mgalvin@tribevr.io>
2020-12-11 12:40:00 -0600

    can render 1 object and style but haven't figured out how to map the data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 import './App.css';
+import Character from './components/Character'
+
+
+
+const URL = 'https://swapi.dev/api/people/';
+const charNum = 1;
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [characters, setCharacters]= useState('');
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // const newArray = [
+  //   characters[0],
+  //   characters[1],
+  //   characters[2],
+  //   characters[3],
+  //   characters[4],
+  //   characters[5],
+  //   characters[6],
+  //   characters[7],
+  //   characters[8],
+  //   characters[9],
+  //  ]
+
+  //  console.log('new array', newArray);
+  
+
+  useEffect( () => { 
+      axios
+      .get(`${URL}1`)
+      .then( (res) => {
+        setCharacters(res.data);
+        console.log(characters);
+      })
+      .catch( (err) => {
+        // debugger;
+      })
+  }, []);
+
+//   useEffect( () => { 
+//     const fetchChar = (charID) => {
+//     axios
+//     .get(`${URL}`)
+//     .then( (res) => {
+//       setCharacters(res.data.map(char => {
+//         return charID === char.id
+//         ?{...char}
+//         : char;
+//       }));
+//       console.log(characters);
+//     })
+//     .catch( (err) => {
+//       debugger;
+//     })
+//   }
+//   fetchChar();
+// }, []);
+
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        // newArray.map(char => {
+        //   return <Character key={char.name} characters={char}/>;
+        // })
+      }   
+      <Character characters={characters}/>
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,72 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Character = (props) => {
+    const { characters } = props
+ 
+    // if (!props.dailyPhoto) return <StyledLoading>Loading...</StyledLoading>;
+
+
+
+    console.log('data', characters)
+
+
+    return (
+    <div className='characters'>
+        <StyledChar>
+            <CharTitle> {characters.name} </CharTitle>
+            <div>
+                <div>
+                    <TraitTitle>General Information</TraitTitle>
+                    <StyledAppearance>
+                        <h4> Birth:  {characters.birth_year} </h4>
+                        <h4> Gender:  {characters.gender} </h4>
+                    </StyledAppearance>
+                </div>
+                <div>
+                    <TraitTitle>Appearance</TraitTitle>
+                    <StyledAppearance>
+                        <h4> Height:  {characters.height} </h4>
+                        <h4> Mass:  {characters.mass} </h4>
+                        <h4> Hair:  {characters.hair_color} </h4>
+                        <h4> Skin:  {characters.skin_color} </h4>
+                        <h4> Eyes:  {characters.eye_color} </h4>
+                    </StyledAppearance>
+                </div>
+            </div>
+        </StyledChar>
+    </div>
+
+    )
+}
+
+const StyledChar = styled.div`
+    background-color: black;
+    color: white;
+    opacity: .7;
+    width: 50%;
+    margin: 0 auto;
+    padding-top: 1px;
+`
+
+const CharTitle = styled.h2`
+    color: #FFE81F;
+`
+const TraitTitle = styled.h3`
+    font-size: 1.2rem;
+    text-decoration: underline;
+`
+
+
+const StyledAppearance = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    width: 80%;
+    margin: 0 auto;
+
+`
+
+
+export default Character;
